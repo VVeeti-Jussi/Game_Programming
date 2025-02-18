594d2c0d4dc92bb1c66d879f1517c1d29c215355
VVeeti-Jussi <veeti.tohni@gmail.com>
2025-02-18 11:39:45 +0200

    object findings

--- Assets/Scripts/hit.cs
+++ Assets/Scripts/hit.cs
@@ -3,8 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
+namespace AE3401
+{ 
+
 public class hit : MonoBehaviour
 {
+    [SerializeField] private string tagName = "Player";
+    [SerializeField] private string tagName2 = "EditorOnly";
+        private string newTag;
+    List<string> list = new List<string>();
     
     void Start()
     {
@@ -13,7 +20,25 @@ public class hit : MonoBehaviour
             if (other.gameObject.tag == "enemy")
             {
                 transform.position = new Vector3(2, 2, 0);
+
             }
         }
+        if (GameObject.FindGameObjectWithTag(tagName))
+        {
+            list.Add("player");
+        }
+
+        if (GameObject.FindGameObjectWithTag(tagName2))
+        {
+            list.Add("quad");
+            foreach (string s in list)
+                {
+                    Debug.Log(s);
+                    newTag = s;
+                    newTag = transform.gameObject.tag = "enemy";
+                }
+        }
+
     }
+}
 }

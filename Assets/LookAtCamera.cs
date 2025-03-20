012444f52d77c515069407c127c3b3aa8b1f3e6c
VVeeti-Jussi <veeti.tohni@gmail.com>
2025-03-20 16:19:44 +0200

    Enemy sprite

---
+++ Assets/LookAtCamera.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    [SerializeField] private Camera _mainCamera;
+
+    private void LateUpdate()
+    {
+        Vector3 cameraPosition = _mainCamera.transform.position;
+
+        cameraPosition.y = transform.position.y;
+
+        transform.LookAt(cameraPosition);
+
+        transform.Rotate(0f, 180f, 0f);
+    }
+}
